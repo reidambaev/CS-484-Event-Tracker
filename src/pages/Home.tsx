f029140faba3bebad3359143cf3d4d6bbfbaf9a3
agustinayala <aayala40@uic.edu>
2025-11-19 10:31:17 -0600

    functionality to map

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,77 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CreateEventModal from "../components/CreateEventModal";
+import { useLoadScript, GoogleMap, Marker, InfoWindow } from "@react-google-maps/api";
+import supabase from "../utils/supabase";
 
-function Home() {
+type Event = {
+  id: string;            
+  title: string;
+  description: string;
+  location: string;
+  latitude: number | null;
+  longitude: number | null;
+  date: string;
+  start_time: string;
+  end_time: string;
+  max_capacity: number;
+  attendee_count: number
+};
+
+export default function Home() {
+  const {isLoaded} = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_PUBLIC_GOOGLE_MAPS_KEY
+  });
+
   const [showModal, setShowModal] = useState(false);
+  const [events, setEvents] = useState<Event[]>([]); 
+  const [clickedEvent, setClickedEvent] = useState<Event | null>(null);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const { data, error } = await supabase.from("events").select("*");
+      if (error) {
+        console.error("Unable to fetch events:", error);
+      } 
+      else if (data) {
+        //ensures only events with coordinates make the list (errors will popup otherwise)
+        const eventsWithCoordinates = data.filter(
+          (e) => e.latitude !== null && e.longitude !== null
+        );
+        setEvents(eventsWithCoordinates);
+      }
+    };
+    fetchEvents();
+  }, []);
+
+  if(!isLoaded) return <div> Currently loading......</div>
+
+  //when a user clicks on a marker, sets the event for info window
+  const handleMarkerClick = (event: Event) => {setClickedEvent(event)}
+
+  //calculates how many spots are left for the clicked event
+  const spotsLeft = clickedEvent && (clickedEvent.max_capacity - clickedEvent.attendee_count)
+
+  //make the window popup for when an event is clicked (Needs work)
+  const infoWindow = clickedEvent && (<InfoWindow
+    onCloseClick={() => setClickedEvent(null)}
+    position= {{lat: clickedEvent.latitude!, lng: clickedEvent.longitude!}}
+    >
+      <div className="p-4">
+        <h4 className="text-sm text-center">{clickedEvent.title}</h4>
+        <p className="text-sm">Date: {clickedEvent.date}</p>
+        <p className="text-sm">Time: {clickedEvent.start_time} - {clickedEvent.end_time}</p>
+        <p className="text-sm">Spots left: {spotsLeft} </p>
+        <p className="text-sm">About: {clickedEvent.description}</p>
+      </div>
+    </InfoWindow>
+
+  )
 
   return (
     <div className="p-8">
-      <h1 className="text-3xl mb-4">Home</h1>
-      <p>Welcome to the event tracker</p>
+      <h1 className="text-3xl mb-4 text-center">Welcome CampusCircuit</h1>
+      <p>Check out the current campus events!</p>
 
       <button
         onClick={() => setShowModal(true)}
@@ -17,15 +80,22 @@ function Home() {
         Create Event
       </button>
 
-      <div className="mt-4">
-        <Link to="/profile" className="text-blue-500 underline mr-4">
-          Profile
-        </Link>
-        <Link to="/admin" className="text-blue-500 underline">
-          Admin
-        </Link>
-      </div>
-
+      <div className="w-full h-screen">
+      <GoogleMap
+        mapContainerStyle={{ width: "100%", height: "500px" }}
+        center={{ lat: 41.872219, lng: -87.649204 }}
+        zoom={17}
+      >
+        {events.map((event) => (
+          <Marker
+            key={event.id}
+            position={{ lat: event.latitude!, lng: event.longitude! }}
+            onClick={() => handleMarkerClick(event)}
+          />
+        ))}
+        {infoWindow}
+      </GoogleMap>
+    </div>
       <CreateEventModal
         isOpen={showModal}
         onClose={() => setShowModal(false)}
@@ -34,4 +104,4 @@ function Home() {
   );
 }
 
-export default Home;
+
